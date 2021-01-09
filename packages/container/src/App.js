4f1adab300d44753c1e01243af2cb79da0dada8d
Vilzz <n79613928275@gmail.com>
2021-01-09 16:26:04 +0400

    Fix classnames issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+  })
   return (
     <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </Router>
   )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+  })
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <Router>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
